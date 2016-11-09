6793905f202ff2ef01d8ccd57fb8e0b9801f550f
Lion001 <noundou.tchounga@gmail.com>
2016-11-09 15:13:30 -0400

    creation des classes

---
+++ TP-Pokemon-Solution/TP-Pokemon/Monster.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Pokemon
+{
+    class Monster
+    {
+        public string nameMonster;
+        public string descripMonster;
+
+
+        public void addMonster()
+        {
+            Console.WriteLine("c'est quoi le nom du monstre?");
+
+        }
+
+        public void deleteMonster()
+        {
+            Console.Read();
+
+        }
+
+        public void modifyMonster()
+        {
+            Console.Read();
+
+        }
+    }
+}
